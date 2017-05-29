c167cd1fefb93d0f15751422542cd7e888b7e73e
Dimitris Tsolis <dimitris.re@gmail.com>
2017-05-29 15:45:13 +0300

    Initial commit

---
+++ recfun/src/main/scala/recfun/Main.scala
@@ -0,0 +1,107 @@
+HSBR_BRAIN_DOC.name = "HSBR_BRAIN_DOC"
+HSBR_BRAIN_DOC.description = "π.χ σχετικές ιατρικές εξετάσεις και το ενημερωτικό νοσηλείας, αν έχει πραγματοποιηθεί."
+HSBR_BRAIN_DOC.value= "ΕΓΓΡΑΦΑ ΓΙΑ ΝΟΣΗΜΑΤΑ ΕΓΚΕΦΑΛΟΥ"
+
+HSEN_ENDCR_DOC.name = "HSEN_ENDCR_DOC"
+HSEN_ENDCR_DOC.description = "π.χ. ορμονολογικές, μαγνητική τομογραφία υπόφυσης, αξονική ή μαγνητική τομογραφία επινεφριδίων, κ.λ.π." 
+HSEN_ENDCR_DOC.value= "ΕΓΓΡΑΦΑ ΓΙΑ ΕΝΔΟΚΡΙΝΟΛΟΓΙΚΑ"
+
+HS_TRYROID_DOC.name = "HS_TRYROID_DOC"
+HS_TRYROID_DOC.description = "π.χ. T3, T4, TSH, παραθορμόνη, ασβέστιο, υπερηχογράφημα θυρεοειδούς, πόρισμα ιστολογικής εξέτασης, κ.λ.π"
+HS_TRYROID_DOC.value= "ΕΓΓΡΑΦΑ ΓΙΑ ΘΥΡΕΟΕΙΔΗΣ"
+
+HSRI_RINO_DOC.name = "HSRI_RINO_DOC"
+HSRI_RINO_DOC.description = "σχετικές ιατρικές εξετάσεις - πορίσματα ιστολογικών εξετάσεων, κ.λπ."
+HSRI_RINO_DOC.value= "ΕΓΓΡΑΦΑ ΓΙΑ ΩΡΛ"
+
+HSAN_ANAPN_DOC.name = "HSAN_ANAPN_DOC"
+HSAN_ANAPN_DOC.description = "π.χ. σπιρομέτρηση, ακτινογραφία ή αξονική τομογραφία θώρακος, μελέτη ύπνου, κ.λπ."
+HSAN_ANAPN_DOC.value= "ΕΓΓΡΑΦΑ ΓΙΑ ΑΝΑΠΝΕΥΣΤΙΚΟ"
+
+HSHR_HEART_DOC.name = "HSHR_HEART_DOC"
+HSHR_HEART_DOC.description = "π.χ. τεστ κοπώσεως, triplex καρδιάς, Holter ρυθμού, σχετικές εξετάσεις αίματος, κ.λπ. "
+HSHR_HEART_DOC.value= "ΕΓΓΡΑΦΑ ΓΙΑ ΝΟΣΗΜΑΤΑ ΚΑΡΔΙΑΣ"
+
+HSCIR_CIRCU_DOC.name = "HSCIR_CIRCU_DOC"
+HSCIR_CIRCU_DOC.description = "π.χ. σχετικές εξετάσεις αίματος, Triplex φλεβών κάτω άκρων, κ.λπ."
+HSCIR_CIRCU_DOC.value= "ΕΓΓΡΑΦΑ ΓΙΑ ΚΥΚΛΟΦΟΡΙΚΟ"
+
+HSBL_BLOODPRESS_DOC.name = "HSBL_BLOODPRESS_DOC"
+HSBL_BLOODPRESS_DOC.description = "π.χ. ηλεκτροκαρδιογράφημα, triplex καρδιάς, ακτινογραφία θώρακος, Holter πιέσεως, κ.λπ."
+HSBL_BLOODPRESS_DOC.value= "ΕΓΓΡΑΦΑ ΓΙΑ ΑΡΤΗΡΙΑΚΗ ΠΙΕΣΗ"
+
+HSGAS_GASTR_DOC.name = "HSGAS_GASTR_DOC"
+HSGAS_GASTR_DOC.description = "π.χ. πόρισμα και τυχόν αποτελέσματα από ιστολογική εξέταση γαστροσκόπησης - κολονοσκόπησης, υπερηχογράφημα ή μαγνητική ή αξονική άνω-κάτω κοιλίας, κ.λπ."
+HSGAS_GASTR_DOC.value= "ΕΓΓΡΑΦΑ ΓΙΑ ΓΑΣΤΡΕΝΤΕΡΟΛΟΓΙΚΑ"
+
+HSUR_URINARY_DOC_M.name = "HSUR_URINARY_DOC_M"
+HSUR_URINARY_DOC_M.description = "π.χ. υπερηχογράφημα νεφρών - ουρητήρων - κύστεως - προστάτη, γενική ούρων, κ.λπ."
+HSUR_URINARY_DOC_M.value= "ΕΓΓΡΑΦΑ ΓΙΑ ΟΥΡΟΠΟΙΗΤΙΚΟ"
+
+HSURI_URINARY_DOC_F.name = "HSURI_URINARY_DOC_F"
+HSURI_URINARY_DOC_F.description = "π.χ. υπερηχογράφημα νεφρών -ουρητήρων - κύστεως, γενική ούρων κ.λπ. "
+HSURI_URINARY_DOC_F.value= "ΕΓΓΡΑΦΑ ΓΙΑ ΟΥΡΟΠΟΙΗΤΙΚΟ"
+
+HFSGEN_GENITAL_DOC_M.name = "HSGEN_GENITAL_DOC_M"
+HSGEN_GENITAL_DOC_M.description = "π.χ. triplex οσχέου υπερηχο προστατη ή αιματολογικες που αφορούν τον προστατη κ.λπ. "
+HSGEN_GENITAL_DOC_M.value= "HSGEN_GENITAL_DOC_M"
+
+HSGEN_GENITAL_DOC_F.name = "HSGEN_GENITAL_DOC_F"
+HSGEN_GENITAL_DOC_F.description = "π.χ. υπερηχογράφημα έσω γεννητικών οργάνων - μαστών, αιματολογικές εξετάσεις, κ.λπ. "
+HSGEN_GENITAL_DOC_F.value= "HSGEN_GENITAL_DOC_F"
+
+HSMY_MYOSKEL_DOC.name = "HSMY_MYOSKEL_DOC"
+HSMY_MYOSKEL_DOC.description = "π.χ. μαγνητική ή/και αξονική τομογραφία, ακτινογραφίες, ηλεκτρομυογράφημα, κ.λπ." 
+HSMY_MYOSKEL_DOC.value= "HSMY_MYOSKEL_DOC"
+
+HSRE_REUM_DOC.name = "HSRE_REUM_DOC"
+HSRE_REUM_DOC.description = "τα αποτελέσματα των σχετικών ιατρικών εξετάσεων"
+HSRE_REUM_DOC.value= "HSRE_REUM_DOC"
+
+HSSK_SKIN_DOC.name = "HSSK_SKIN_DOC"
+HSSK_SKIN_DOC.description = "π.χ. πορίσματα ιστολογικών εξετάσεων, χαρτογράφηση σπίλων, κ.λπ." 
+HSSK_SKIN_DOC.value= "HSSK_SKIN_DOC"
+
+HSLEM_LEMFAD_DOC.name = "HSLEM_LEMFAD_DOC"
+HSLEM_LEMFAD_DOC.description = "ο ιατρικός έλεγχος που διαπιστώθηκε το νόσημα"
+HSLEM_LEMFAD_DOC.value= "HSLEM_LEMFAD_DOC"
+
+HSCA_CANCER_DOC.name = "HSCA_CANCER_DOC"
+HSCA_CANCER_DOC.description = "π.χ. αποτελέσματα ιστολογικής εξέτασης, μαγνητική ή αξονική τομογραφία, καρκινικούς δείκτες, κ.λπ." 
+HSCA_CANCER_DOC.value= "HSCA_CANCER_DOC"
+
+HS_DISSEX_DOC.name = "HS_DISSEX_DOC"
+HS_DISSEX_DOC.description = "o ιατρικός ελεγχος που διαπιστώθηκε το νόσημα"
+HS_DISSEX_DOC.value= "HS_DISSEX_DOC"
+
+HSDIAB_DIABETE_DOC.name = "HSDIAB_DIABETE_DOC"
+HSDIAB_DIABETE_DOC.description = "τα αποτελέσματα του σακχάρου αίματος νηστείας, το αποτέλεσμα της γλυκοζυλιωμένης αιμοσφαιρίνης καθώς και τυχόν αποτελέσματα καμπύλης σακχάρου ή και ινσουλίνης." 
+HSDIAB_DIABETE_DOC.value= "HSDIAB_DIABETE_DOC"
+
+HSDIS_DISABILITY_DOC.name = "HSDIS_DISABILITY_DOC"
+HSDIS_DISABILITY_DOC.description = "τα αποτελέσματα των σχετικών ιατρικών εξετάσεων" 
+HSDIS_DISABILITY_DOC.value= "HSDIS_DISABILITY_DOC"
+
+HIS_NOS_DOC.name = "HIS_NOS_DOC"
+HIS_NOS_DOC.description = "π.χ. αναλυτικό ιατρικό ιστορικό νοσηλείας, πρακτικό χειρουργείου, αποτελέσματα ιατρικών εξετάσεων"
+HIS_NOS_DOC.value= "HIS_NOS_DOC"
+
+HIS_EXAM1_DOC.name = "HIS_EXAM1_DOC"
+HIS_EXAM1_DOC.description = "τα αποτελέσματα των τελευταίων ιατρικών εξετάσεων"
+HIS_EXAM1_DOC.value= "HIS_EXAM1_DOC"
+
+HS_DISSEX_2_DOC.name = "HS_DISSEX_2_DOC"
+HS_DISSEX_2_DOC.description = "o ιατρικός έλεγχος που διαπιστωθηκε το νόσημα"
+HS_DISSEX_2_DOC.value= "HS_DISSEX_2_DOC"
+
+HSDIAGN_NOSHMATA_DOC.name = "HSDIAGN_NOSHMATA_DOC"
+HSDIAGN_NOSHMATA_DOC.description = "o ιατρικός έλεγχος που διαπιστωθηκε το νόσημα"
+HSDIAGN_NOSHMATA_DOC.value= "HSDIAGN_NOSHMATA_DOC"
+
+HSDIAGNDIS_DISABILITY_DOC.name = "HSDIAGNDIS_DISABILITY_DOC"
+HSDIAGNDIS_DISABILITY_DOC.description = "τα αποτέλεσματα των ιατρικών εξέτασεων"
+HSDIAGNDIS_DISABILITY_DOC.value= "HSDIAGNDIS_DISABILITY_DOC"
+
+HIS_DRUG_DOC.name = "HIS_DRUG_DOC"
+HIS_DRUG_DOC.description = "τα αποτελέσματα των εξετάσεων που οδήγησαν σε φαρμακευτική αγωγή"
+HIS_DRUG_DOC.value= "HIS_DRUG_DOC"
